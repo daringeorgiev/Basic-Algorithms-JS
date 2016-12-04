bef0aef23a69713b37685f934f62b70758d804b4
Darin Georgiev <georgiev.darin@gmail.com>
2016-12-04 23:15:23 +0200

    Add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,27 @@ import React, {Component} from 'react';
 class footer extends Component {
   render() {
     return (
-      <div className='container-fluid'>
-        footer
-      </div>
+      <footer className='container text-center'>
+        <small>
+          <strong>
+            <a href='https://github.com/daringeorgiev/Basic-Algorithms-JS' target='_blank'>
+              Open Source Project
+            </a>
+          </strong>
+          <br />
+
+          <strong>
+            powered by:&nbsp;
+          </strong>
+          <a href='https://facebook.github.io/react/' target='_blank'>ReactJS</a>
+          <br />
+
+          <strong>
+            Author:&nbsp;
+          </strong>
+          <a href='https://daringeorgiev.github.io/' target='_blank'>Darin Georgiev</a>
+        </small>
+      </footer>
     );
   }
 }
