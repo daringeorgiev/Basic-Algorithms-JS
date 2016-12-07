b188cbc1de89c79b47e131d6c490543862bd3189
Darin Georgiev <georgiev.darin@gmail.com>
2016-12-07 02:03:33 +0200

    Fix footer position

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class footer extends Component {
   render() {
     return (
-      <footer className='text-center'>
+      <footer id='footer' className='footer text-center'>
         <small>
           <strong>
             <a href='https://github.com/daringeorgiev/Basic-Algorithms-JS' target='_blank'>
