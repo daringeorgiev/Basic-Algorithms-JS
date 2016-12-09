ebe5f5cf1b5ffa74c67001fc8672ce6678484ea9
Darin Georgiev <georgiev.darin@gmail.com>
2016-12-10 01:28:30 +0200

    Add github gist wrapper

---
+++ src/components/gist.jsx
@@ -0,0 +1,49 @@
+import React, {Component, PropTypes} from 'react';
+
+class gist extends Component {
+  componentDidMount() {
+    this._updateIframeContent();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.id !== nextProps.id;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this._updateIframeContent();
+  }
+
+  _updateIframeContent() {
+    let iframe = this.refs.iframe;
+
+    let doc = iframe.document;
+    if (iframe.contentDocument) {
+      doc = iframe.contentDocument;
+    } else if (iframe.contentWindow) {
+      doc = iframe.contentWindow.document;
+    }
+
+    let gistScript = '<script type="text/javascript" src="https://gist.github.com/' + this.props.id + '.js"></script>';
+    let styles = '<style>*{font-size:12px;}</style>';
+    let resizeScript = 'onload="parent.document.getElementById(\'gist-' + this.props.id + '\').style.height=document.body.scrollHeight + \'px\'"';
+    let iframeHtml = '<html><head><base target="_parent">' + styles + '</head><body ' + resizeScript + '>' + gistScript + '</body></html>';
+
+    doc.open();
+    doc.writeln(iframeHtml);
+    doc.close();
+  }
+
+  render() {
+    return (
+      <div>
+        <iframe id={'gist-' + this.props.id} ref='iframe' width='100%' frameBorder='0' />
+      </div>
+    );
+  }
+}
+
+gist.propTypes = {
+  id: React.PropTypes.string.isRequired
+};
+
+export default gist;
--- src/routes/sorting/bubble-sort.jsx
+++ src/routes/sorting/bubble-sort.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Gist from '../../components/gist.jsx'
 
 class bubbleSort extends Component {
   render() {
     return (
       <div>
-        bubbleSort
+        <Gist id='973520942aa88f1a5b739623383b238d' />
       </div>
     );
   }
